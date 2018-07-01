1762251375f4bb85ce090e119eacf86942b8044f
DariuszWlodarz <dariuszwlodarz@gmail.com>
2018-07-01 14:17:25 +0200

    api call with rx

--- src/index.js
+++ src/index.js
@@ -1,8 +1,54 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from "react"
+import { render } from "react-dom"
+import { Observable } from "rxjs"
+import config from "recompose/rxjsObservableConfig"
+import {
+  setObservableConfig,
+  componentFromStream
+} from "recompose"
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+setObservableConfig(config)
+
+const personById = id =>
+  `https://swapi.co/api/people/${id}`
+
+
+const Card = props => (
+  <div>
+    <h1>{props.name}</h1>
+    <h2>{props.homeworld}</h2>
+  </div>
+)
+
+const loadById = id =>
+  Observable.ajax(personById(id))
+    .pluck("response")
+    .switchMap(
+      response =>
+        Observable.ajax(response.homeworld)
+          .pluck("response")
+          .startWith({ name: "" }),
+      (person, homeworld) => ({
+        ...person,
+        homeworld: homeworld.name
+      })
+    )
+
+const CardStream = componentFromStream(props$ =>
+  props$
+    .switchMap(props => loadById(props.id))
+    .map(Card)
+)
+
+const App = () => (
+  <div>
+    <Card name="John" homeworld="Earth"/>
+    <CardStream id={1}/>
+    <CardStream id={12}/>
+    <CardStream id={10}/>
+    <CardStream id={24}/>
+  </div>
+)
+render(
+  <App/>, document.getElementById("root"))
+
